637c11f7927303ddebcee3b91b2cdc57aeadd37a
Valerii Rozuvan <vrozu@softserveinc.com>
2025-03-19 13:10:19 +0000

    move db init logic to separate end point

--- server/index.mjs
+++ server/index.mjs
@@ -1,5 +1,5 @@
-import express from 'express';
-import pg from 'pg';
+const express = require('express');
+const pg = require('pg');
 import {Connector} from '@google-cloud/cloud-sql-connector';
 
 const {Pool} = pg;
@@ -23,19 +23,39 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/db', async (req, res) => {
-  await pool.query('INSERT INTO visits(created_at) VALUES(NOW())');
-  const {rows} = await pool.query('SELECT created_at FROM visits ORDER BY created_at DESC LIMIT 5');
-  console.table(rows); // prints the last 5 visits
-  res.send(rows);
+app.get('/db-init', async () => {
+  try {
+    await pool.query(`CREATE TABLE IF NOT EXISTS visits (
+      id SERIAL NOT NULL,
+      created_at timestamp NOT NULL,
+      PRIMARY KEY (id)
+    );`);
+  } catch (e) {
+    res.send(JSON.stringify(e));
+    return;
+  }
+
+  res.send('DB initialized OK');
+});
+
+app.get('/db-test', async (req, res) => {
+  let response;
+  try {
+    await pool.query('INSERT INTO visits(created_at) VALUES(NOW())');
+    response = await pool.query('SELECT created_at FROM visits ORDER BY created_at DESC LIMIT 5');
+  } catch (e) {
+    res.send(JSON.stringify(e));
+    return;
+  }
+
+  if (response && response.rows) {
+    res.send(response.rows);
+  } else {
+    res.send('no data')
+  }
 });
 
 app.listen(port, '0.0.0.0', async () => {
   console.log('process.env: ', process.env);
-  await pool.query(`CREATE TABLE IF NOT EXISTS visits (
-    id SERIAL NOT NULL,
-    created_at timestamp NOT NULL,
-    PRIMARY KEY (id)
-  );`);
   console.log(`helloworld: listening on port ${port}`);
 });
