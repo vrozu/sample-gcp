b5e0e798893bd93a14da28f280c3d59f164fdf14
Valerii Rozuvan <vrozu@softserveinc.com>
2025-03-19 15:53:19 +0000

    we do not care about ssl for the moment

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ const { Pool } = pg;
     pool = new Pool({
       ...clientOpts,
       type: 'postgres',
+      ssl: { rejectUnauthorized: false },
       database: process.env.DB_NAME,      // 'postgres'
       user: process.env.DB_USER,          // 'postgres'
       // password: process.env.DB_PASSWORD,  // 'password'
